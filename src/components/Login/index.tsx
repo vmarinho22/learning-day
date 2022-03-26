a57835b0d649e9eebcbaed7845b398ee3f2aa20a
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-03-26 13:42:28 -0300

    fix(login): changing animations

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,6 +1,13 @@
-import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack
+} from "@mui/material";
 import type { FC } from "react";
-import FadeIn from 'react-fade-in';
 import { BiHide, BiShow } from "react-icons/bi";
 import useLogin from "./useLogin";
 
@@ -14,50 +21,51 @@ const Login: FC = () => {
 
   return (
     <form className="w-full">
-      <FadeIn transitionDuration={1000}>
-        <FormControl variant="outlined" fullWidth size="small" >
-          <InputLabel htmlFor="user">Usuário</InputLabel>
-          <OutlinedInput 
-            id="user" 
-            size="small" 
-            label="Usuário"
-            fullWidth 
-            autoComplete="username" 
-            
-          />
-        </FormControl>
-      </FadeIn>
-      <FadeIn transitionDuration={1000}>
-        <FormControl variant="outlined" fullWidth size="small" margin="normal">
-          <InputLabel htmlFor="password">Senha</InputLabel>
-          <OutlinedInput 
-            id="password" 
-            type={showPassword ? "text" : "password"}
-            size="small" 
-            label="Senha"
-            fullWidth 
-            autoComplete="password" 
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <BiHide /> : <BiShow />}
-                </IconButton>
-              </InputAdornment>
-            } 
-          />
-        </FormControl>
-      </FadeIn>
+      <FormControl variant="outlined" fullWidth size="small">
+        <InputLabel htmlFor="user">Usuário</InputLabel>
+        <OutlinedInput
+          id="user"
+          size="small"
+          label="Usuário"
+          fullWidth
+          autoComplete="username"
+        />
+      </FormControl>
+
+      <FormControl variant="outlined" fullWidth size="small" margin="normal">
+        <InputLabel htmlFor="password">Senha</InputLabel>
+        <OutlinedInput
+          id="password"
+          type={showPassword ? "text" : "password"}
+          size="small"
+          label="Senha"
+          fullWidth
+          autoComplete="password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <BiHide /> : <BiShow />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+
       <br />
-      <FadeIn transitionDuration={1000}>
-        <Stack spacing={2} direction="row">
-          <Button className="text-base uppercase text-white bg-pr-purple hover:bg-pr-ocean hover:text-black transition ease-in-out delay-50 duration-300" variant="contained">Entrar</Button>
-        </Stack>
-      </FadeIn>
+
+      <Stack spacing={2} direction="row">
+        <Button
+          className="w-full mb-1 mt-1 text-base uppercase text-white bg-pr-purple hover:bg-pr-ocean hover:text-black transition ease-in-out delay-50 duration-300"
+          variant="contained"
+        >
+          Entrar
+        </Button>
+      </Stack>
     </form>
   );
 };
