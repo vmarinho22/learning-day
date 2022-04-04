5e2899afc55128ee3c990db737560883d576579b
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-04-03 21:08:39 -0300

    fix(graphql): fix options method

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -9,6 +9,12 @@ const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
 
 export default cors(async function handler(req: any, res: any) {
+
+  if(req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
+  
   await startServer;
 
   await apolloServer.createHandler({
